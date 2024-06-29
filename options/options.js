8e971e8fb0e130f11fd213aa8394fb9cd8ca9fd3
Crowdbotics <team@crowdbotics.com>
2024-06-29 15:44:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snowy-waterfall-48583",
+  url: "https://snowy-waterfall-48583.botics.co",
+  api: "https://snowy-waterfall-48583.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
